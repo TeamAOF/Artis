89c126d550ad28fc157a89946c3fd6a347dd6db7
Meredith Espinosa <goclonefilms@gmail.com>
2019-10-25 22:09:21 -0700

    include libgui/staticdata, remove test code

--- src/main/resources/data/artis/tweakers/tweaker_test.js
+++
@@ -1,5 +0,0 @@
-var TableTweaker = ArtisTweaker.get("artis:test_table");
-
-TableTweaker.addShaped([["minecraft:iron_ingot", "minecraft:iron_ingot"],
-                        ["minecraft:diamond", "minecraft:diamond"]],
-                        "minecraft:diamond@4", "minecraft:diamond_pickaxe", 3);
