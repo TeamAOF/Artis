1ac6659fae20c7deb81d5290e97f976b22444a81
Jacob Farley <jacobfarley2017@gmail.com>
2020-07-23 23:44:22 -0500

    Not sure why these weren't included.

--- src/main/java/io/github/alloffabric/artis/inventory/slot/ValidatedArtisResultSlot.java
+++ src/main/java/io/github/alloffabric/artis/inventory/slot/ValidatedArtisResultSlot.java
@@ -1,10 +1,9 @@
-package io.github.alloffabric.artis.inventory;
+package io.github.alloffabric.artis.inventory.slot;
 
 import io.github.alloffabric.artis.api.ArtisCraftingRecipe;
 import io.github.alloffabric.artis.api.SpecialCatalyst;
+import io.github.alloffabric.artis.inventory.ArtisCraftingInventory;
 import io.github.cottonmc.cotton.gui.ValidatedSlot;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
@@ -16,104 +15,105 @@ import net.minecraft.util.collection.DefaultedList;
 
 public class ValidatedArtisResultSlot extends ValidatedSlot {
 
-	private final ArtisCraftingInventory craftingInv;
-	private final PlayerEntity player;
-	private int amount;
-	private int syncId;
+    private final ArtisCraftingInventory craftingInv;
+    private final PlayerEntity player;
+    private int amount;
+    private final int syncId;
 
-	public ValidatedArtisResultSlot(PlayerEntity player, ArtisCraftingInventory inventory, Inventory inventoryIn, int index, int xPosition, int yPosition, int syncId) {
-		super(inventoryIn, index, xPosition, yPosition);
-		this.player = player;
-		this.craftingInv = inventory;
-		this.syncId = syncId;
-	}
+    public ValidatedArtisResultSlot(PlayerEntity player, ArtisCraftingInventory inventory, Inventory inventoryIn, int index, int xPosition, int yPosition, int syncId) {
+        super(inventoryIn, index, xPosition, yPosition);
+        this.player = player;
+        this.craftingInv = inventory;
+        this.syncId = syncId;
+    }
 
-	@Override
-	public boolean canInsert(ItemStack stack) {
-		return false;
-	}
+    @Override
+    public boolean canInsert(ItemStack stack) {
+        return false;
+    }
 
-	@Override
-	public ItemStack takeStack(int amount) {
-		if (this.hasStack()) {
-			this.amount += Math.min(amount, this.getStack().getCount());
-		}
+    @Override
+    public ItemStack takeStack(int amount) {
+        if (this.hasStack()) {
+            this.amount += Math.min(amount, this.getStack().getCount());
+        }
 
-		return super.takeStack(amount);
-	}
+        return super.takeStack(amount);
+    }
 
-	@Override
-	protected void onCrafted(ItemStack stack, int amount) {
-		this.amount += amount;
-		this.onCrafted(stack);
-	}
+    @Override
+    protected void onCrafted(ItemStack stack, int amount) {
+        this.amount += amount;
+        this.onCrafted(stack);
+    }
 
-	@Override
-	protected void onTake(int amount) {
-		this.amount += amount;
-	}
+    @Override
+    protected void onTake(int amount) {
+        this.amount += amount;
+    }
 
-	@Override
-	protected void onCrafted(ItemStack stack) {
-		if (this.amount > 0) {
-			stack.onCraft(this.player.world, this.player, this.amount);
-		}
+    @Override
+    protected void onCrafted(ItemStack stack) {
+        if (this.amount > 0) {
+            stack.onCraft(this.player.world, this.player, this.amount);
+        }
 
-		if (this.inventory instanceof RecipeUnlocker) {
-			((RecipeUnlocker) this.inventory).unlockLastRecipe(this.player);
-		}
+        if (this.inventory instanceof RecipeUnlocker) {
+            ((RecipeUnlocker) this.inventory).unlockLastRecipe(this.player);
+        }
 
-		this.amount = 0;
-	}
+        this.amount = 0;
+    }
 
-	@Override
-	public ItemStack onTakeItem(PlayerEntity player, ItemStack stack) {
-		this.onCrafted(stack);
-		DefaultedList<ItemStack> remainders = player.world.getRecipeManager().getRemainingStacks(craftingInv.getType(), this.craftingInv, player.world);
+    @Override
+    public ItemStack onTakeItem(PlayerEntity player, ItemStack stack) {
+        this.onCrafted(stack);
+        DefaultedList<ItemStack> remainders = player.world.getRecipeManager().getRemainingStacks(craftingInv.getType(), this.craftingInv, player.world);
 
-		for (int i = 0; i < remainders.size() - 1; ++i) {
-			ItemStack input = this.craftingInv.getStack(i);
-			ItemStack remainder = remainders.get(i);
-			if (!input.isEmpty()) {
-				this.craftingInv.removeStack(i, 1);
-				input = this.craftingInv.getStack(i);
-			}
+        for (int i = 0; i < remainders.size() - 1; ++i) {
+            ItemStack input = this.craftingInv.getStack(i);
+            ItemStack remainder = remainders.get(i);
+            if (!input.isEmpty()) {
+                this.craftingInv.removeStack(i, 1);
+                input = this.craftingInv.getStack(i);
+            }
 
-			if (!remainder.isEmpty()) {
-				if (input.isEmpty()) {
-					this.craftingInv.setStack(i, remainder);
-				} else if (ItemStack.areItemsEqualIgnoreDamage(input, remainder) && ItemStack.areTagsEqual(input, remainder)) {
-					remainder.increment(input.getCount());
-					this.craftingInv.setStack(i, remainder);
-				} else if (!this.player.inventory.insertStack(remainder)) {
-					this.player.dropItem(remainder, false);
-				}
-			}
-		}
+            if (!remainder.isEmpty()) {
+                if (input.isEmpty()) {
+                    this.craftingInv.setStack(i, remainder);
+                } else if (ItemStack.areItemsEqualIgnoreDamage(input, remainder) && ItemStack.areTagsEqual(input, remainder)) {
+                    remainder.increment(input.getCount());
+                    this.craftingInv.setStack(i, remainder);
+                } else if (!this.player.inventory.insertStack(remainder)) {
+                    this.player.dropItem(remainder, false);
+                }
+            }
+        }
 
-		if (this.inventory instanceof RecipeUnlocker) {
-			Recipe lastRecipe = ((RecipeUnlocker) inventory).getLastRecipe();
-			if (lastRecipe instanceof ArtisCraftingRecipe) {
-				ArtisCraftingRecipe recipe = (ArtisCraftingRecipe) lastRecipe;
-				int catalystSlot = remainders.size() - 1;
-				ItemStack remainder = remainders.get(catalystSlot).copy();
-				if (!remainder.isEmpty()) {
-					this.craftingInv.setStack(catalystSlot, remainder);
-				} else {
-					ItemStack catalyst = this.craftingInv.getCatalyst().copy();
-					if (catalyst.isDamageable()) {
-						catalyst.damage(recipe.getCatalystCost(), craftingInv.getPlayer(), (user) -> user.sendToolBreakStatus(user.getActiveHand()));
-					} else if (catalyst.getItem() instanceof SpecialCatalyst) {
-						catalyst = ((SpecialCatalyst)catalyst.getItem()).consume(catalyst, recipe.getCatalystCost());
-					} else {
-						catalyst.decrement(recipe.getCatalystCost());
-					}
-					this.craftingInv.setStack(catalystSlot, catalyst);
-					if (!player.world.isClient) ServerSidePacketRegistry.INSTANCE.sendToPlayer(player, new ScreenHandlerSlotUpdateS2CPacket(syncId, 37, catalyst));
-				}
-			}
-		}
+        if (this.inventory instanceof RecipeUnlocker) {
+            Recipe lastRecipe = ((RecipeUnlocker) inventory).getLastRecipe();
+            if (lastRecipe instanceof ArtisCraftingRecipe) {
+                ArtisCraftingRecipe recipe = (ArtisCraftingRecipe) lastRecipe;
+                int catalystSlot = remainders.size() - 1;
+                ItemStack remainder = remainders.get(catalystSlot).copy();
+                if (!remainder.isEmpty()) {
+                    this.craftingInv.setStack(catalystSlot, remainder);
+                } else {
+                    ItemStack catalyst = this.craftingInv.getCatalyst().copy();
+                    if (catalyst.isDamageable()) {
+                        catalyst.damage(recipe.getCatalystCost(), craftingInv.getPlayer(), (user) -> user.sendToolBreakStatus(user.getActiveHand()));
+                    } else if (catalyst.getItem() instanceof SpecialCatalyst) {
+                        catalyst = ((SpecialCatalyst) catalyst.getItem()).consume(catalyst, recipe.getCatalystCost());
+                    } else {
+                        catalyst.decrement(recipe.getCatalystCost());
+                    }
+                    this.craftingInv.setStack(catalystSlot, catalyst);
+                    if (!player.world.isClient)
+                        ServerSidePacketRegistry.INSTANCE.sendToPlayer(player, new ScreenHandlerSlotUpdateS2CPacket(syncId, 37, catalyst));
+                }
+            }
+        }
 
-		return stack;
-	}
+        return stack;
+    }
 }
--- src/main/java/io/github/alloffabric/artis/inventory/slot/WArtisResultSlot.java
+++ src/main/java/io/github/alloffabric/artis/inventory/slot/WArtisResultSlot.java
@@ -1,34 +1,28 @@
-package io.github.alloffabric.artis.inventory;
+package io.github.alloffabric.artis.inventory.slot;
 
-import com.google.common.collect.Lists;
-import io.github.cottonmc.cotton.gui.GuiDescription;
+import io.github.alloffabric.artis.inventory.ArtisCraftingInventory;
 import io.github.cottonmc.cotton.gui.ValidatedSlot;
 import io.github.cottonmc.cotton.gui.widget.WItemSlot;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
-import net.minecraft.screen.slot.Slot;
-
-import java.util.List;
 
 public class WArtisResultSlot extends WItemSlot {
 
-	private final PlayerEntity player;
-	private final ArtisCraftingInventory craftingInventory;
-	private Inventory inventory;
-	private int syncId;
+    private final PlayerEntity player;
+    private final ArtisCraftingInventory craftingInventory;
+    private final Inventory inventory;
+    private final int syncId;
 
-	public WArtisResultSlot(PlayerEntity player, ArtisCraftingInventory craftingInventory, Inventory resultInv, int startIndex, int slotsWide, int slotsHigh, boolean big, int syncId) {
-		super(resultInv, startIndex, slotsWide, slotsHigh, big);
-		this.player = player;
-		this.craftingInventory = craftingInventory;
-		this.inventory = resultInv;
-		this.syncId = syncId;
-	}
+    public WArtisResultSlot(PlayerEntity player, ArtisCraftingInventory craftingInventory, Inventory resultInv, int startIndex, int slotsWide, int slotsHigh, boolean big, int syncId) {
+        super(resultInv, startIndex, slotsWide, slotsHigh, big);
+        this.player = player;
+        this.craftingInventory = craftingInventory;
+        this.inventory = resultInv;
+        this.syncId = syncId;
+    }
 
-	@Override
-	protected ValidatedSlot createSlotPeer(Inventory inventory, int index, int x, int y) {
-		return new ValidatedArtisResultSlot(player, craftingInventory, this.inventory, index, x, y, syncId);
-	}
+    @Override
+    protected ValidatedSlot createSlotPeer(Inventory inventory, int index, int x, int y) {
+        return new ValidatedArtisResultSlot(player, craftingInventory, this.inventory, index, x, y, syncId);
+    }
 }
